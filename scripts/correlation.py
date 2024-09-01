0c2b50549c0b5d74046dbdda6de35f823f01d44f
Ephrem Mekonnen <ephrem8118@gmail.com>
2024-09-01 15:21:59 +0300

    correlation python file code debugging

--- scripts/correlation.py
+++ scripts/correlation.py
@@ -1,5 +1,5 @@
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 
 
 # class Correlation:
@@ -24,29 +24,29 @@ import numpy as np
 #         self.data.sort_index(inplace=True)
 
 
-class Correlation:
-    def __init__(self, file_paths):
-        self.data = None
-        if file_paths:
-            self.load_data(file_paths)
+# class Correlation:
+#     def __init__(self, file_paths):
+#         self.data = None
+#         if file_paths:
+#             self.load_data(file_paths)
 
-    def load_data(self, file_paths):
-        data_frames = []
-        for file_path in file_paths:
-            df = pd.read_csv(file_path, parse_dates=['Date'], index_col='Date')
-            data_frames.append(df)
-        self.data = pd.concat(data_frames)
-        self.data.sort_index(inplace=True)
+#     def load_data(self, file_paths):
+#         data_frames = []
+#         for file_path in file_paths:
+#             df = pd.read_csv(file_path, parse_dates=['Date'], index_col='Date')
+#             data_frames.append(df)
+#         self.data = pd.concat(data_frames)
+#         self.data.sort_index(inplace=True)
 
-    def align_by_date(df1, df2):
-    # Ensure both DataFrames have Date as index
-    if df1.index.name != 'Date':
-        df1.set_index('Date', inplace=True)
-    if df2.index.name != 'Date':
-        df2.set_index('Date', inplace=True)
+#     def align_by_date(df1, df2):
+#     # Ensure both DataFrames have Date as index
+#     if df1.index.name != 'Date':
+#         df1.set_index('Date', inplace=True)
+#     if df2.index.name != 'Date':
+#         df2.set_index('Date', inplace=True)
     
-    # Align datasets by merging on the Date index
-    aligned_data = pd.merge(df1, df2, left_index=True, right_index=True, how='inner')
+#     # Align datasets by merging on the Date index
+#     aligned_data = pd.merge(df1, df2, left_index=True, right_index=True, how='inner')
     
-    return aligned_data
+#     return aligned_data
 
